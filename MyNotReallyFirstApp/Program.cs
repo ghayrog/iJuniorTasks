5910dddcafb2ca03d64cb2034e6f004bb9114ac5
ghayrog <91064841+ghayrog@users.noreply.github.com>
2022-06-07 22:26:59 +0500

    Добавьте файлы проекта.

---
+++ MyNotReallyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string personName = "Ibrahim";
+        bool isWearingCandibober = false;
+        int personAge = 39;
+        float personHeight = 180.5f;
+        double personRotationAngle = 90.000001;
+        uint personNameLength = (uint)personName.Length;
+        char firstChar = personName[0];
+        sbyte personIQ = -10;
+        long personMoneyAmount = 1240939453;
+        byte firstCharIndex = (byte)firstChar;
+    }
+}
